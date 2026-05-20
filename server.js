00a44e3b71f04c214bf9ee23838a1f44399c0112
Meeannu12 <anuragkumar007812@gmail.com>
2026-05-20 12:19:17 +0530

    add some chnage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello this side anurag kumar 🚀");
+    res.send("Heelo sir  🚀");
 });
 
 app.listen(3000, () => {
